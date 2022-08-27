f5a7eecc4338278a86c30444ffdbe5aca6d5e23f
Maxim <you@example.com>
2022-08-27 14:19:51 +0300

    com1

---
+++ tsk1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter a count of numbers ");
+int numbers = int.Parse(Console.ReadLine());
+
+int[] massive = new int[numbers];
+
+   void InputNumbers(int numbers)
+   {
+   for (int i = 0; i < numbers; i++)
+     {
+      massive[i] = int.Parse(Console.ReadLine());
+     }
+   }
+
+
+   int massive2(int[] massive)
+   {
+     int count = 0;
+     for (int i = 0; i < massive.Length; i++)
+     {
+       if(massive[i] > 0 ) 
+       count = count + 1; 
+     }
+     return count;
+   }
+
+   InputNumbers(numbers);
+
+
+Console.WriteLine($"Count of numbers > 0 --- {massive2(massive)} ");
---
+++ tsk2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a b1 number");
+double b1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a k1 number");
+double k1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a b2 number ");
+double b2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a k2 number");
+double k2 = int.Parse(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"The point of intersection of two straight lines:\n;({x};{y})");
